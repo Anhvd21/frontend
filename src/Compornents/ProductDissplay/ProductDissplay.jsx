36df9be40e55172ffd0a96de0e51da26f5848c71
Anhvd21 <anhv5209@gmail.com>
2023-12-12 01:18:55 +0700

    csspdisplay

--- src/Compornents/ProductDissplay/ProductDissplay.jsx
+++ src/Compornents/ProductDissplay/ProductDissplay.jsx
@@ -4,21 +4,21 @@ import star_icon from "../Assets/star_icon.png";
 const ProductDissplay = (props) => {
     const {product} = props;
     return (
-        <div className='productdissplay'>
-            <div className="productdissplay_left">
-                <div className="productdissplay_img_list">
+        <div className='productdisplay'>
+            <div className="productdisplay_left">
+                <div className="productdisplay_img_list">
                     <img src={product.image} alt="" />
                     <img src={product.image} alt="" />
                     <img src={product.image} alt="" />
                     <img src={product.image} alt="" />
                 </div>
-                <div className="productdissplay_img">
-                    <img className="productdissplay_main_img" src={product.image} alt="" />
+                <div className="productdisplay_img">
+                    <img className="productdisplay_main_img" src={product.image} alt="" />
                 </div>
             </div>
-            <div className="productdissplay_right">
+            <div className="productdisplay_right">
                 <h1> {product.name} </h1>
-                <div className="productdissplay_right_star">
+                <div className="productdisplay_right_star">
                     <img src={star_icon} alt="" />
                     <img src={star_icon} alt="" />
                     <img src={star_icon} alt="" />
@@ -33,19 +33,21 @@ const ProductDissplay = (props) => {
                 <div className="productdisplay-right-description">
                 Feel free to customize the template based on the specific details of the shirt you're describing. Include information about the fabric, color, design features, and any unique qualities that make the shirt stand out.
                 </div>
-            </div>
-            <div className="productdisplay-right-size" >
+                <div className="productdisplay-right-size" >
                 <h1>Select Size</h1>
-                <div className="productdisplay-size" ></div>
-                <div>S</div>
-                <div>M</div>
-                <div>L</div>
-                <div>XL</div>
-                <div>XXL</div>
+                <div className="productdisplay-size" >
+                    <div>S</div>
+                    <div>M</div>
+                    <div>L</div>
+                    <div>XL</div>
+                    <div>XXL</div>
+                </div>
             </div>
             <button>ADD TO CART</button>
             <p className="productdisplay-right-category"><span>Category :</span> Woman, T-Shirt, Crop Top</p>
             <p className="productdisplay-right-category"><span>Tags :</span> Mordern, Lastest</p>
+            </div>
+
         </div>
         
     )
